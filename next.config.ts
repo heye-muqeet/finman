a505c62f886c009cf56d9d6bda869333e5788d0f
engr-muqeet-scope <abdul.muqeet@scopeinspectapp.com>
2025-11-25 04:22:41 +0500

    chore: remove Cloudinary domain from image configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Image configuration for Cloudinary
   images: {
-    domains: ['res.cloudinary.com'],
     remotePatterns: [
       {
         protocol: 'https',
